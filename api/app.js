e9a6b2d1e2cd52a977f50eba6b1778c9f0a6a817
Pradeep <pradeep99909@gmail.com>
2024-05-26 22:53:49 +0530

    cors added

--- api/app.js
+++ api/app.js
@@ -1,7 +1,9 @@
 const serverStart = () => {
+  const cors = require("cors");
   const express = require("express");
   const app = express();
   const router = require("./routes/routes");
+  app.use(cors());
   app.use(express.json());
   app.use("/api/v1", router);
   app.listen(8080, () => {
