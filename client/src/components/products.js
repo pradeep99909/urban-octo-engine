ac483e224889a47f1add50cd7efd3e03e7ccc0ed
Pradeep <pradeep99909@gmail.com>
2024-05-26 22:35:31 +0530

    className error

--- client/src/components/products.js
+++ client/src/components/products.js
@@ -5,16 +5,16 @@ import Network from "../network";
 function Product() {
   const { setCartOpen, setCarts } = useContext(AppContext);
   return (
-    <a href="#" class="group">
-      <div class="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
+    <a href="#" className="group">
+      <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
         <img
           src="https://tailwindui.com/img/ecommerce-images/category-page-04-image-card-01.jpg"
           alt="Tall slender porcelain bottle with natural clay textured body and cork stopper."
-          class="h-full w-full object-cover object-center group-hover:opacity-75"
+          className="h-full w-full object-cover object-center group-hover:opacity-75"
         />
       </div>
-      <h3 class="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
-      <p class="mt-1 text-lg font-medium text-gray-900">$48</p>
+      <h3 className="mt-4 text-sm text-gray-700">Earthen Bottle</h3>
+      <p className="mt-1 text-lg font-medium text-gray-900">$48</p>
       <button
         onClick={async () => {
           const api = new Network.cartNetwork.default("pradeep");
@@ -32,11 +32,12 @@ function Product() {
           };
           await api.addNewCart(cart);
           const usersCart = await api.getUserCart();
+          console.log("🚀 ~ onClick={ ~ usersCart:", usersCart);
           setCarts(usersCart);
           setCartOpen(true);
         }}
         type="button"
-        class="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+        className="inline-flex items-center rounded-md bg-indigo-600 px-3 py-2 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
       >
         Add To Cart
       </button>
@@ -47,11 +48,11 @@ function Product() {
 class Products extends React.Component {
   render() {
     return (
-      <div class="bg-white">
-        <div class="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
-          <h2 class="sr-only">Products</h2>
+      <div className="bg-white">
+        <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
+          <h2 className="sr-only">Products</h2>
 
-          <div class="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
+          <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8">
             <Product />
             <Product />
             <Product />
