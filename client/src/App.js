e56009ff3ed8d5fa3eacf9cc73ca396e12965677
Pradeep <pradeep99909@gmail.com>
2024-05-26 14:55:48 +0530

    orders and order list

--- client/src/App.js
+++ client/src/App.js
@@ -2,11 +2,13 @@ import "./App.css";
 import Header from "./components/header";
 import Products from "./components/products";
 import Cart from "./components/cart";
+import Orders from "./components/order";
+import OrderList from "./components/order.list";
 function App() {
   return (
     <div className="App">
       <Header />
-      <Products />
+      <OrderList />
     </div>
   );
 }
