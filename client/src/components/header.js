43bc946e23603b27790cebaa058591d47d8a43e1
Pradeep <pradeep99909@gmail.com>
2024-05-26 14:01:37 +0530

    headers changes

--- client/src/components/header.js
+++ client/src/components/header.js
@@ -1,4 +1,14 @@
+// import React from "react";
+
 import React from "react";
+import {
+  Disclosure,
+  Menu,
+  MenuButton,
+  MenuItem,
+  MenuItems,
+  Transition,
+} from "@headlessui/react";
 
 function BrandName() {
   return (
@@ -11,31 +21,26 @@ function BrandName() {
     </div>
   );
 }
-
 class Options extends React.Component {
   render() {
     return (
       <div className="hidden md:block">
         <div className="ml-10 flex items-baseline space-x-4">
-          <a
-            href="#"
-            className="bg-gray-900 text-white rounded-md px-3 py-2 text-sm font-medium"
-            aria-current="page"
-          >
-            Products
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-          >
-            Cart
-          </a>
-          <a
-            href="#"
-            className="text-gray-300 hover:bg-gray-700 hover:text-white rounded-md px-3 py-2 text-sm font-medium"
-          >
-            Orders
-          </a>
+          {navigation.map((item) => (
+            <a
+              key={item.name}
+              href={item.href}
+              className={classNames(
+                item.current
+                  ? "bg-gray-900 text-white"
+                  : "text-gray-300 hover:bg-gray-700 hover:text-white",
+                "rounded-md px-3 py-2 text-sm font-medium"
+              )}
+              aria-current={item.current ? "page" : undefined}
+            >
+              {item.name}
+            </a>
+          ))}
         </div>
       </div>
     );
@@ -44,200 +49,92 @@ class Options extends React.Component {
 
 function Avatar() {
   return (
-    <div className="relative ml-3">
-      <div>
-        <button
-          type="button"
-          className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-          id="user-menu-button"
-          aria-expanded="false"
-          aria-haspopup="true"
+    <div className="ml-4 flex items-center md:ml-6">
+      {/* Profile dropdown */}
+      <Menu as="div" className="relative ml-3">
+        <div>
+          <MenuButton className="relative flex max-w-xs items-center rounded-full bg-gray-800 text-sm focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800">
+            <span className="absolute -inset-1.5" />
+            <span className="sr-only">Open user menu</span>
+            <img className="h-8 w-8 rounded-full" src={user.imageUrl} alt="" />
+          </MenuButton>
+        </div>
+        <Transition
+          enter="transition ease-out duration-100"
+          enterFrom="transform opacity-0 scale-95"
+          enterTo="transform opacity-100 scale-100"
+          leave="transition ease-in duration-75"
+          leaveFrom="transform opacity-100 scale-100"
+          leaveTo="transform opacity-0 scale-95"
         >
-          <span className="absolute -inset-1.5"></span>
-          <span className="sr-only">Open user menu</span>
-          <img
-            className="h-8 w-8 rounded-full"
-            src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-            alt=""
-          />
-        </button>
-      </div>
-
-      <div
-        className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none"
-        role="menu"
-        aria-orientation="vertical"
-        aria-labelledby="user-menu-button"
-        tabindex="-1"
-      >
-        <a
-          href="#"
-          className="block px-4 py-2 text-sm text-gray-700"
-          role="menuitem"
-          tabindex="-1"
-          id="user-menu-item-0"
-        >
-          Your Profile
-        </a>
-        <a
-          href="#"
-          className="block px-4 py-2 text-sm text-gray-700"
-          role="menuitem"
-          tabindex="-1"
-          id="user-menu-item-1"
-        >
-          Settings
-        </a>
-        <a
-          href="#"
-          className="block px-4 py-2 text-sm text-gray-700"
-          role="menuitem"
-          tabindex="-1"
-          id="user-menu-item-2"
-        >
-          Sign out
-        </a>
-      </div>
+          <MenuItems className="absolute right-0 z-10 mt-2 w-48 origin-top-right rounded-md bg-white py-1 shadow-lg ring-1 ring-black ring-opacity-5 focus:outline-none">
+            {userNavigation.map((item) => (
+              <MenuItem key={item.name}>
+                {({ focus }) => (
+                  <a
+                    href={item.href}
+                    className={classNames(
+                      focus ? "bg-gray-100" : "",
+                      "block px-4 py-2 text-sm text-gray-700"
+                    )}
+                  >
+                    {item.name}
+                  </a>
+                )}
+              </MenuItem>
+            ))}
+          </MenuItems>
+        </Transition>
+      </Menu>
     </div>
   );
 }
 
-class Navigation extends React.Component {
+const user = {
+  name: "Tom Cook",
+  email: "tom@example.com",
+  imageUrl:
+    "https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80",
+};
+const navigation = [
+  { name: "Products", href: "#", current: true },
+  { name: "Cart", href: "#", current: false },
+  { name: "Orders", href: "#", current: false },
+];
+const userNavigation = [
+  { name: "Your Profile", href: "#" },
+  { name: "Settings", href: "#" },
+  { name: "Sign out", href: "#" },
+];
+
+function classNames(...classes) {
+  return classes.filter(Boolean).join(" ");
+}
+
+export default class Header extends React.Component {
   render() {
     return (
-      <nav className="bg-gray-800">
-        <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
-          <div className="flex h-16 items-center justify-between">
-            <div className="flex items-center">
-              <BrandName />
-              <Options />
-            </div>
-            <div className="hidden md:block">
-              <div className="ml-4 flex items-center md:ml-6">
-                <Avatar />
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="md:hidden" id="mobile-menu">
-          <div className="space-y-1 px-2 pb-3 pt-2 sm:px-3">
-            <a
-              href="#"
-              className="bg-gray-900 text-white block rounded-md px-3 py-2 text-base font-medium"
-              aria-current="page"
-            >
-              Dashboard
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Team
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Projects
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Calendar
-            </a>
-            <a
-              href="#"
-              className="text-gray-300 hover:bg-gray-700 hover:text-white block rounded-md px-3 py-2 text-base font-medium"
-            >
-              Reports
-            </a>
-          </div>
-          <div className="border-t border-gray-700 pb-3 pt-4">
-            <div className="flex items-center px-5">
-              <div className="flex-shrink-0">
-                <img
-                  className="h-10 w-10 rounded-full"
-                  src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
-                  alt=""
-                />
-              </div>
-              <div className="ml-3">
-                <div className="text-base font-medium leading-none text-white">
-                  Tom Cook
+      <>
+        <div className="min-h-full">
+          <Disclosure as="nav" className="bg-gray-800">
+            {({ open }) => (
+              <>
+                <div className="mx-auto max-w-7xl px-4 sm:px-6 lg:px-8">
+                  <div className="flex h-16 items-center justify-between">
+                    <div className="flex items-center">
+                      <BrandName />
+                      <Options />
+                    </div>
+                    <div className="hidden md:block">
+                      <Avatar />
+                    </div>
+                  </div>
                 </div>
-                <div className="text-sm font-medium leading-none text-gray-400">
-                  tom@example.com
-                </div>
-              </div>
-              <button
-                type="button"
-                className="relative ml-auto flex-shrink-0 rounded-full bg-gray-800 p-1 text-gray-400 hover:text-white focus:outline-none focus:ring-2 focus:ring-white focus:ring-offset-2 focus:ring-offset-gray-800"
-              >
-                <span className="absolute -inset-1.5"></span>
-                <span className="sr-only">View notifications</span>
-                <svg
-                  className="h-6 w-6"
-                  fill="none"
-                  viewBox="0 0 24 24"
-                  stroke-width="1.5"
-                  stroke="currentColor"
-                  aria-hidden="true"
-                >
-                  <path
-                    stroke-linecap="round"
-                    stroke-linejoin="round"
-                    d="M14.857 17.082a23.848 23.848 0 005.454-1.31A8.967 8.967 0 0118 9.75v-.7V9A6 6 0 006 9v.75a8.967 8.967 0 01-2.312 6.022c1.733.64 3.56 1.085 5.455 1.31m5.714 0a24.255 24.255 0 01-5.714 0m5.714 0a3 3 0 11-5.714 0"
-                  />
-                </svg>
-              </button>
-            </div>
-            <div className="mt-3 space-y-1 px-2">
-              <a
-                href="#"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-              >
-                Your Profile
-              </a>
-              <a
-                href="#"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-              >
-                Settings
-              </a>
-              <a
-                href="#"
-                className="block rounded-md px-3 py-2 text-base font-medium text-gray-400 hover:bg-gray-700 hover:text-white"
-              >
-                Sign out
-              </a>
-            </div>
-          </div>
+              </>
+            )}
+          </Disclosure>
         </div>
-      </nav>
+      </>
     );
   }
 }
-
-class Header extends React.Component {
-  render() {
-    return (
-      <div className="min-h-full">
-        <Navigation />
-        <header className="bg-white shadow">
-          <div className="mx-auto max-w-7xl px-4 py-6 sm:px-6 lg:px-8">
-            <h1 className="text-3xl font-bold tracking-tight text-gray-900">
-              Dashboard
-            </h1>
-          </div>
-        </header>
-        <main>
-          <div className="mx-auto max-w-7xl py-6 sm:px-6 lg:px-8"></div>
-        </main>
-      </div>
-    );
-  }
-}
-
-export default Header;
