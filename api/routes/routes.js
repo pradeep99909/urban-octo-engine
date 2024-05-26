a6b418dd3c71fd3e890735075248d8ced54e680b
Pradeep <pradeep99909@gmail.com>
2024-05-26 17:39:17 +0530

    cart api route added

--- api/routes/routes.js
+++ api/routes/routes.js
@@ -3,7 +3,7 @@ const router = express.Router();
 const routes = require("./");
 
 //router.use("/admin", routes.adminRoutes);
-//router.use("/cart", routes.cartRoutes);
+router.use("/cart", routes.cartRoutes);
 router.use("/order", routes.orderRoutes);
 
 module.exports = router;
