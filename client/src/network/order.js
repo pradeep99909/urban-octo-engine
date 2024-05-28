20de072e7ec0cf3a3a715548d2e1b360358121c5
Pradeep <pradeep99909@gmail.com>
2024-05-29 00:00:40 +0530

    verify api

--- client/src/network/order.js
+++ client/src/network/order.js
@@ -2,22 +2,25 @@ const { default: Api } = require("./api");
 
 class Cart {
   constructor(userId) {
-    this.api = new Api("http://localhost:8080/api/v1");
+    this.api = new Api("http://localhost:8080/api/v1/order");
   }
   async getUserOrder() {
-    return this.api.getData("/order");
+    return this.api.getData("/");
   }
   async addNewOrder() {
-    return this.api.postData("/order");
+    return this.api.postData("/");
   }
   async getOrder(orderId) {
-    return this.api.getData("/order/" + orderId);
+    return this.api.getData("/" + orderId);
   }
   async updateCart(orderId, body) {
-    return this.api.updateDate("/order/" + orderId, body);
+    return this.api.updateDate("/" + orderId, body);
   }
   async cancelOrder(orderId) {
-    return this.api.deletData("/order/" + orderId);
+    return this.api.deletData("/" + orderId);
+  }
+  async verifyCoupon(body) {
+    return this.api.postData("/verify", body);
   }
 }
 
